4f665bf28e97351e7611772ea6478c4f4c913906
junwen.luo <luojunwenbashen@163.com>
2018-01-14 21:52:18 +0800

    integrate with membership

--- ClaimBasedIdentities/ClaimBasedIdentities/Controllers/AuthController.cs
+++ ClaimBasedIdentities/ClaimBasedIdentities/Controllers/AuthController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IdentityModel.Services;
 using System.IdentityModel.Tokens;
 using System.Linq;
 using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Profile;
 using System.Web.Security;
 using ClaimBasedIdentities.Models;
 
@@ -25,19 +27,12 @@ namespace ClaimBasedIdentities.Controllers
 		{
 			if (ModelState.IsValid)
 			{
-				if (FormsAuthentication.Authenticate(model.UserName, model.Password))
+				//if (FormsAuthentication.Authenticate(model.UserName, model.Password))
+				if (Membership.ValidateUser(model.UserName, model.Password))
 				{
-					var claims = new[]
-					{
-						new Claim(ClaimTypes.NameIdentifier, model.UserName),
-						new Claim(ClaimTypes.Name, model.UserName)
-					};
-					var identity = new ClaimsIdentity(claims, "Forms");
-					var principal = new ClaimsPrincipal(identity);
+					var user = Membership.GetUser(model.UserName);
 
-					var token = new SessionSecurityToken(principal);
-
-					FederatedAuthentication.SessionAuthenticationModule.WriteSessionTokenToCookie(token);
+					signIn(user);
 
 					return Redirect(FormsAuthentication.DefaultUrl);
 				}
@@ -47,10 +42,66 @@ namespace ClaimBasedIdentities.Controllers
 			return View(model);
 		}
 
-	    public ActionResult SignOut()
+		[HttpGet]
+		public ActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Register(RegisterModel model)
+		{
+			FederatedAuthentication.SessionAuthenticationModule.SignOut();
+
+			if (ModelState.IsValid)
+			{
+				if (model.Password != model.RePassword)
+				{
+					ModelState.AddModelError("RepeatPasswordError", "Could not verify password...");
+					return View(model);
+				}
+
+				var user = Membership.CreateUser(model.UserName, model.Password, model.Email);
+				var profile = ProfileBase.Create(user.UserName, true);
+				profile.SetPropertyValue("Organization", model.Organization);
+				profile.Save();
+
+				signIn(user);
+
+				return Redirect(FormsAuthentication.DefaultUrl);
+			}
+			return View(model);
+		}
+
+
+		public ActionResult SignOut()
 	    {
 		    FederatedAuthentication.SessionAuthenticationModule.SignOut();
 		    return RedirectToAction("Login");
 	    }
+
+		private void signIn(MembershipUser user)
+		{
+			var claims = new[]
+					{
+						new Claim(ClaimTypes.NameIdentifier, user.Email),
+						new Claim(ClaimTypes.Name, user.UserName)
+					};
+
+			var profile = ProfileBase.Create(user.UserName, true);
+			var claimList = new List<Claim>(claims);
+			foreach (SettingsProperty property in ProfileBase.Properties)
+			{
+				claimList.Add(new Claim(property.Attributes["CustomProviderData"].ToString()
+					, profile[property.Name].ToString()));
+			}
+
+			var identity = new ClaimsIdentity(claimList, "Forms");
+			var principal = new ClaimsPrincipal(identity);
+
+			var token = new SessionSecurityToken(principal);
+
+			FederatedAuthentication.SessionAuthenticationModule.WriteSessionTokenToCookie(token);
+		}
     }
 }
---
+++ ClaimBasedIdentities/ClaimBasedIdentities/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClaimBasedIdentities.Models
+{
+	public class RegisterModel
+	{
+		public string UserName { get; set; }
+
+		public string Email { get; set; }
+
+		public string Password { get; set; }
+
+		public string RePassword { get; set; }
+
+		public string Organization { get; set; }
+	}
+}
