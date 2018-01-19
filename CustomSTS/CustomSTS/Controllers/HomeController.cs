d303b095da4b2d6cf65f091ea0988a6fc98f2f4e
junwen.luo <luojunwenbashen@163.com>
2018-01-19 19:37:07 +0800

    implement STS

--- CustomSTS/CustomSTS/Controllers/HomeController.cs
+++ CustomSTS/CustomSTS/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Configuration;
+using System.IdentityModel.Services;
+using System.IdentityModel.Tokens;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,11 +25,21 @@ namespace CustomSTS.Controllers
 
 				if (action == SignIn)
 				{
-					//var formData = 
+					var formData = ProcessSignIn(Request.Url, (ClaimsPrincipal) User);
 					return new ContentResult() { ContentType = "text/html" };
 				}
 			}
 			return View();
         }
-    }
+
+		private static string ProcessSignIn(Uri url, ClaimsPrincipal user)
+		{
+			var requestMessage = (SignInRequestMessage)WSFederationMessage.CreateFromUri(url);
+			var signingCredentials = new X509SigningCredentials(CustomSecurityTokenService.GetCertificate(ConfigurationManager.AppSettings["SigningCertificateName"]));
+			var config = new SecurityTokenServiceConfiguration(ConfigurationManager.AppSettings["IssuerName"], signingCredentials);
+			var sts = new CustomSecurityTokenService(config);
+			var responseMessage = FederatedPassiveSecurityTokenServiceOperations.ProcessSignInRequest(requestMessage, user, sts);
+			return responseMessage.WriteFormPost();
+		}
+	}
 }
